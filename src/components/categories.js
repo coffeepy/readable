deb35e2c49fc74f8f255eb611389bbc880cab473
coffeepy <goddardbr@gmail.com>
2017-11-21 13:40:31 -0600

    refactor: removed unnecesarry code from categories

--- src/components/categories.js
+++ src/components/categories.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react'
-import { getAllCats } from '../backendAPI'
 import Category from './Category'
 // Categories component used on main page for choosing what categorie you want to view
 class Categories extends Component {
